6a963bf7bd1249634b56fb85a5f577f408159d17
Jeremy Poinas <poinas.jeremy@gmail.com>
2023-07-31 14:56:27 +0200

    Encryption + Decryption

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
@@ -5,20 +5,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String operation = scanner.nextLine();
         String input = scanner.nextLine();
         int key = scanner.nextInt();
-        StringBuilder output = new StringBuilder();
+        String output = "";
 
-        for (int i = 0; i < input.length(); i++) {
-            char curr = input.charAt(i);
-            if (curr < 'a' || curr > 'z') {
-                output.append(curr);
-            } else {
-                char newChar = alphabet.charAt((alphabet.indexOf(curr) + key) % 26);
-                output.append(newChar);
-            }
+        if (operation.equals("enc")) {
+            output = encode(input, key);
+        } else if (operation.equals("dec")) {
+            output = decode(input, key);
         }
         System.out.println(output);
+    }
+
+    private static String encode(String input, int key) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            int curr = input.charAt(i);
+            char newChar = (char) (curr + key);
+            output.append(newChar);
+        }
+        return new String(output);
+    }
+
+    private static String decode(String input, int key) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            int curr = input.charAt(i);
+            char newChar = (char) (curr - key);
+            output.append(newChar);
+        }
+        return new String(output);
     }
 }
