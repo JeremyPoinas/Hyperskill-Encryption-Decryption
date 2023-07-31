e7ca851e23a397b8b7061dbaf3e8c06cc6752cd2
Jeremy Poinas <poinas.jeremy@gmail.com>
2023-07-31 12:13:58 +0200

    Implemented Caesar Cypher

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
@@ -1,9 +1,13 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String alphabet2 = "zyxwvutsrqponmlkjihgfedcba";
-        String input = "we found a treasure!";
+        Scanner scanner = new Scanner(System.in);
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String input = scanner.nextLine();
+        int key = scanner.nextInt();
         StringBuilder output = new StringBuilder();
 
         for (int i = 0; i < input.length(); i++) {
@@ -11,7 +15,7 @@ public class Main {
             if (curr < 'a' || curr > 'z') {
                 output.append(curr);
             } else {
-                char newChar = alphabet2.charAt((int) curr - 97);
+                char newChar = alphabet.charAt((alphabet.indexOf(curr) + key) % 26);
                 output.append(newChar);
             }
         }
