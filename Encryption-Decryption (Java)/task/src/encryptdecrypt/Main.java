591c0d5fb44915e08ea11dd62ba6f12d9d7bc2c1
Jeremy Poinas <poinas.jeremy@gmail.com>
2023-08-01 10:49:17 +0200

    Reading data to encode from file + writing decoded data to file

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
@@ -1,37 +1,70 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String operation = scanner.nextLine();
-        String input = scanner.nextLine();
-        int key = scanner.nextInt();
-        String output = "";
+        String operation = "enc";
+        int key = 0;
+        String data = "";
+        String in = "";
+        String out = "";
 
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-mode" -> operation = args[i + 1];
+                case "-key" -> key = Integer.parseInt(args[i + 1]);
+                case "-data" -> data = args[i + 1];
+                case "-in" -> in = args[i + 1];
+                case "-out" -> out = args[i + 1];
+            }
+        }
+
+        if (!in.equals("") && data.equals("")) {
+            try {
+                data = new String(Files.readAllBytes(Paths.get(in)));
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+        }
+
+        String output = "";
         if (operation.equals("enc")) {
-            output = encode(input, key);
+            output = encode(data, key);
         } else if (operation.equals("dec")) {
-            output = decode(input, key);
+            output = decode(data, key);
+        }
+
+        if (!out.equals("")) {
+            File outputFile = new File(out);
+            try (PrintWriter printWriter = new PrintWriter(outputFile)) {
+                printWriter.print(output);
+                return;
+            } catch (IOException e) {
+                System.out.print("Error");
+            }
         }
         System.out.println(output);
     }
 
-    private static String encode(String input, int key) {
+    private static String encode(String data, int key) {
         StringBuilder output = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            int curr = input.charAt(i);
+        for (int i = 0; i < data.length(); i++) {
+            int curr = data.charAt(i);
             char newChar = (char) (curr + key);
             output.append(newChar);
         }
         return new String(output);
     }
 
-    private static String decode(String input, int key) {
+    private static String decode(String data, int key) {
         StringBuilder output = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            int curr = input.charAt(i);
+        for (int i = 0; i < data.length(); i++) {
+            int curr = data.charAt(i);
             char newChar = (char) (curr - key);
             output.append(newChar);
         }
